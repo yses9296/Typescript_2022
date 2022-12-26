ee459dbef769da647cd4035c7e5c6f99439081b7
Eunseo Choi <yses9296@gmail.com>
2022-12-26 23:04:43 +0900

    Typescript 05

--- TypeScript-basic/05/tutorial06.ts
+++ TypeScript-basic/05/tutorial06.ts
@@ -1,1 +1,20 @@
 //Intersection Types
+
+interface Boat {
+    name: string;
+    start(): void;
+}
+interface Toy {
+    name: string;
+    color: string;
+    price: number;
+}
+
+// Toy interface with Boat interface
+// 인터페이스의 모든 조건(요소) 충족 해야한다.
+const toyBoat: Toy & Boat = {
+    name: "Titanic",
+    start() {console.log("Merry Christmas")},
+    color: "Blue",
+    price: 30000
+}
